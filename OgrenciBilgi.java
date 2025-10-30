4972bd5d8cf707cc247b453484683d2ff3fc0ab2
Pelinay-kosan <250542002@firat.edu.tr>
2025-10-30 20:00:10 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [PELİNAY KOŞAN]
+ * Ogrenci No: [250542002]
+ * Tarih: [30.11.2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -13,48 +13,47 @@ import java.util.Scanner;
 
 public class OgrenciBilgi {
     public static void main(String[] args) {
-        // Scanner objesi olusturun
         
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Öğrencinin adını giriniz: ");
+        String ad = input.nextLine();
+
+        System.out.println("Öğrencinin soyadını giriniz: ");
+        String soyad = input.nextLine();
+
+        System.out.println("Öğrencinin numarasını giriniz: ");
+        int numara = input.nextInt();
+
+        System.out.print("Öğrencinin yaşını giriniz: ");
+        int yas = input.nextInt();
+
+        System.out.println("Genel Not Ortalamasını giriniz: " );
+        double gno = input.nextDouble();
+
+        System.out.println("\n----- ÖĞRENCİ BİLGİLERİ -----");
+        System.out.printf("%-20s: %s %s\n", "Ad Soyad", ad, soyad);
+        System.out.printf("%-20s: %d\n", "Numara", numara);
+        System.out.printf("%-20s: %d\n", "Yaş", yas);
+        System.out.printf("%-20s: %.2f\n", "Genel Not Ortalaması", gno);
+
+     }
+
+}
         
         
         
+       ----- ÖĞRENCİ BİLGİLERİ -----
+Ad Soyad            : PELİNAY KOŞAN
+Numara              : 250542002
+Yaş                 : 18
+Genel Not Ortalaması: 3,18
+
+Process finished with exit code 0
         
         
         
 
         
         
-        // Scanner'i kapatin (önemli pratik)
         
-    }
-}
